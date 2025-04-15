1b20e31712f21934d3ec5fb3d90503d9833fd41d
Tanzim <tanzim008@gmail.com>
2025-04-15 02:10:00 -0230

    added getAlluser method in user controller

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -1,10 +1,34 @@
 const User = require('./../models/User');
 const Note = require('./../models/Note');
+const asyncHandler = require('express-async-handler');
 
 // Get - Get all users
-const getAllUser = ()=>{
-    
-}
+const getAllUser = asyncHandler(
+    async (req,res)=>{
+        const users = await User.find().select('-password').lean();
+        if(!users?.length){
+            return res.status(400).json({message:`No users found`})
+        } 
+        res.json({message: `${users.length} users found!`});
+    }
+);
 // Post - Create a new user
+const createUser = asyncHandler(
+    (req,res)=>{
+
+    }
+);
 // Patch - update a user
+const updateUser = asyncHandler(
+    (req,res)=>{
+
+    }
+);
 // Delete - delete a user
+const deleteUser = asyncHandler(
+    (req,res)=>{
+
+    }
+);
+
+module.exports = {getAllUser,createUser,updateUser,deleteUser}
